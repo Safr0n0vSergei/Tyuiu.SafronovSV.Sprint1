9e166f2ee712062e959feee3ffcfe4b844a17344
Safr0n0vSergei <144219000+Safr0n0vSergei@users.noreply.github.com>
2023-09-26 22:40:02 +0500

    Add files via upload

---
+++ Tyuiu.SafronovSV.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SafronovSV.Sprint1.Task5.V6.Lib;
+
+namespace Tyuiu.SafronovSV.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService dataService = new DataService();
+            int k = 23, wait = 2, res = dataService.Calculate(k);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
